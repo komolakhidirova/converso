0a2dc68947b1204bb448838fe9f44243cede085f
Komola Khidirova <komolakhidirova@gmail.com>
2025-05-29 17:55:36 +0500

    clerk subscription

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,18 +1,45 @@
 import CompanionForm from '@/components/CompanionForm'
+import { newCompanionPermissions } from '@/lib/actions/companion.actions'
 import { auth } from '@clerk/nextjs/server'
+import Image from 'next/image'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 const NewCompanion = async () => {
 	const { userId } = await auth()
 	if (!userId) redirect('/sign-in')
 
+	const canCreateCompanion = await newCompanionPermissions()
+
 	return (
 		<main className='lg:w-1/3 md:w-2/3 items-center justify-center'>
-			<article className='w-full gap-4 flex flex-col'>
-				<h1>Companion Builder</h1>
+			{canCreateCompanion ? (
+				<article className='w-full gap-4 flex flex-col'>
+					<h1>Companion Builder</h1>
 
-				<CompanionForm />
-			</article>
+					<CompanionForm />
+				</article>
+			) : (
+				<article className='companion-limit'>
+					<Image
+						src='/images/limit.svg'
+						alt='companion limit reached'
+						width={360}
+						height={230}
+					/>
+					<div className='cta-badge'>Upgrade you plan</div>
+					<p>
+						You’ve reached your companion limit. Upgrade to create more
+						companions and premium features.
+					</p>
+					<Link
+						href='/subscription'
+						className='btn-primary w-full justify-center'
+					>
+						Upgrade My Plan
+					</Link>
+				</article>
+			)}
 		</main>
 	)
 }
